8810309cb83e1266d05d7a2b8d7100ee3e18c715
Apoorva <apoorvar.bsc23.rvu.edu.in>
2024-04-06 22:35:10 +0530

    hello

--- class_prac.py
+++ class_prac.py
@@ -11,34 +11,39 @@
 # print(dir(x))
 # x.__class__
 # x.__class__.__class__
-#x.__dict__
+# x.__dict__
 
+
+class x:
+    def __init__(self):
+        self.n=10
 
 # class x:
 #     def __init__(self):
 #         self.n=10
+print(x)
+a=x()
+print(a.n)
 
-# class x:
-#     def __init__(self):
-#         self.n=10
-# print(a.n)
-# x=a()
-# print(x.n)
+# class menu:
+#     def __init__(self) :
+#         self.items={}
+#     def add(self,item,price):
+#         self.items[item]=price
+#     def show(self):
+#         print("Menu:")
+#         for i in self.items:
+#             print(f"{i} = Rs. {self.items.get(i)}")
+# m = menu()
+# m.add("idly",100)
+# m.add("vada",200)
+# m.add("sambar",100)
+# m.add("dosa",150)
+# m.show()
 
-class menu:
-    def __init__(self) :
-        self.items={}
-    def add(self,item,price):
-        self.items[item]=price
-    def show(self):
-        print("Menu:")
-        for i in self.items:
-            print(f"{i} = Rs. {self.items.get(i)}")
-m = menu()
-m.add("idly",100)
-m.add("vada",200)
-m.add("sambar",100)
-m.add("dosa",150)
-m.show()
+
+
+
+
     
 
